a7e3ecfc74a2b977f9424807e87e41cc2fa556e4
jsjtxietian <jsjtxietian@outlook.com>
2021-02-25 14:09:12 +0800

    add ccc and ploycollision

--- build.bat
+++ build.bat
@@ -11,7 +11,7 @@ del /S /Q .\Build
 mkdir .\Build
 
 pushd .\Build
-cl %CommonCompilerFlags% ..\testing.cpp -Fmtesting.map -Fetesting.exe /link %CommonLinkerFlags%
+cl %CommonCompilerFlags% ..\Example_CarCrimeCity.cpp -Fmtesting.map -Fetesting.exe /link %CommonLinkerFlags%
 popd
 
 echo ----------------------
